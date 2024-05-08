9b57464a6ef5fa1ee1b1248342c130ca1a4fe397
Ajay Rathnam <ajay@descpro.com>
2024-05-08 18:22:43 +0530

    changes

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    var typed = new Typed('#typing', {
+        strings: ['Software Developer', 'AI Developer','Poet', 'Artist'],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+});
